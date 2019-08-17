edfb987a19033a351c16dc49c0b32030b2b5ff3c
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2019-08-18 01:23:17 +0700

    start scripting from GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameBoard gameBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupNewGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetupNewGame()
+    {
+        //spawn player
+        gameBoard.SpawnPlayer();
+
+        //spawn an enemy
+        gameBoard.SpawnEnemy();
+
+        //spawn an ally
+        gameBoard.SpawnAlly();
+    }
+}
